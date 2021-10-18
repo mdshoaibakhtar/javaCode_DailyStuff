80b542a90d7acde9a29e14f58aacefb0dc8cf5d7
Md Shoaib Akhtar <86504964+azaan08@users.noreply.github.com>
2021-10-18 12:32:51 +0530

    commited

--- practice.java
+++ practice.java
@@ -5,4 +5,67 @@ public class practice
     {
         System.out.println("Hello World");
     }
+}
+
+package mdshoaib.practice;
+import java.util.Scanner;
+public class practice
+{
+    public static void main(String [] args)
+    {
+        Scanner sc =  new Scanner(System.in);
+        System.out.println("Enter Your Name");
+        String name = sc.nextLine();
+        System.out.println("my name is "+name);
+
+
+    }
 }*/
+
+package mdshoaib.practice;
+import java.util.Scanner;
+
+public class practice
+{
+    static name(String name)
+    {
+
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Lets Begin The Show");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
